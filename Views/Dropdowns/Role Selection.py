01449b3262d37e6276e20ef283818de1b14c9d53
marko! <104887467+DencePixel@users.noreply.github.com>
2023-04-06 18:27:19 +0100

    Update Role Selection.py

--- Views/Dropdowns/Role Selection.py
+++ Views/Dropdowns/Role Selection.py
@@ -1,39 +1,6 @@
 import discord
 from discord.ext import commands
 
-class RoleSelection(discord.ui.RoleSelect):
-    def __init__(self, guild_Roles):
-        super().__init__(placeholder='Select a role!', max_values=1, min_values=1)# Contains the placeholder text which appears on top of the dropdown when not selected, min_values means the minimum amount of roles they need to select, man_values is the maximum amount of roles they can select.
-        self.guild_roles = guild_Roles
-
-    async def callback(self, interaction: discord.Interaction):
-        role_id = int(self.values[0].id)
-        role = interaction.guild.get_role(role_id)
-        guild_id = interaction.guild.id
-        await interaction.response.send_message(f"Looks like you picked {role.mention}!")
-
-@client.command(name="role")
-async def role(ctx):
-    RoleSelection_Dropdown = RoleSelection(ctx.guild.channels)
-    RoleSelection_View = discord.ui.View()
-    RoleSelection_View.add_item(RoleSelection_Dropdown)
-    await ctx.send(f"What role will you pick?", view=RoleSelection_View)
-
-# How To Use In A Cog #
-import discord
-from discord.ext import commands
-
-class RoleSelection(discord.ui.RoleSelect):
-    def __init__(self, guild_Roles):
-        super().__init__(placeholder='Select a role!', max_values=1, min_values=1)# Contains the placeholder text which appears on top of the dropdown when not selected, min_values means the minimum amount of roles they need to select, man_values is the maximum amount of roles they can select.
-        self.guild_roles = guild_Roles
-
-    async def callback(self, interaction: discord.Interaction):
-        role_id = int(self.values[0].id)
-        role = interaction.guild.get_role(role_id)
-        guild_id = interaction.guild.id
-        await interaction.response.send_message(f"Looks like you picked {role.mention}!")
-
 
 class RoleSelection(discord.ui.RoleSelect):
     def __init__(self, guild_Roles):
@@ -44,18 +11,16 @@ class RoleSelection(discord.ui.RoleSelect):
         role_id = int(self.values[0].id)
         role = interaction.guild.get_role(role_id)
         guild_id = interaction.guild.id
-        await interaction.response.send_message(f"You selected the {role.mention}")
+        await interaction.response.send_message(f"Looks like you picked {role.mention}!")
 
-class Role(commands.Cog):
-    def __init__(self, client):
-        self.client = client
 
-    @commands.command(name="role")
-    async def role(self, ctx):
-        RoleSelection_Dropdown = RoleSelection(ctx.guild.channels)
-        RoleSelection_View = discord.ui.View()
-        RoleSelection_View.add_item(RoleSelection_Dropdown)
-        await ctx.send(f"What role will you pick?", view=RoleSelection_View)
+class RoleSelectionView(discord.ui.View):
+    def __init__(self, guild_Roles):
+        super().__init__(placeholder='Select a role!', max_values=1, min_values=1)
+        self.add_item(RoleSelection(guild_Roles))
 
-async def setup(client: commands.Bot) -> None:
-    await client.add_cog(Role(client))
+
+@client.command(name="role")
+async def role(ctx):
+    view = RoleSelectionView(guild_Roles=ctx.guild.roles)
+    await ctx.send(f"What role will you pick?", view=view)
